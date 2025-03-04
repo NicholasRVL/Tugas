b51eead067150ddbb84392a786b947a75b9d12bc
NicholasRVL <nicholasraphaelvalentiuslaw_2428250016@mhs.mdp.ac.id>
2025-03-04 21:40:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let Pro1 = localStorage.getItem("val5")
+let Ttl1 = localStorage.getItem("val1")
+let Dis1 = localStorage.getItem("val3")
+
+document.getElementById("ttl1").innerText = Ttl1
+document.getElementById("dis1").innerText = Dis1
+document.getElementById("pro1").src = Pro1
+
+
+
+
